cfe16ea4ec7f3e530a648d77751a0097e75b90c4
bsbsq <bisaqwi06@gmail.com>
2023-07-03 19:58:58 +0300

    2ed edit

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_midi/flutter_midi.dart';
 import 'package:piano/piano.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -16,6 +17,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String? choice;
+  Uri? link;
   // This widget is the root of your application.
   FlutterMidi flutterMidi = FlutterMidi();
   @override
@@ -31,55 +34,97 @@ class _MyAppState extends State<MyApp> {
         sf2: _byte, name: 'assets/$choice.sf2'.replaceAll('assets/', ''));
   }
 
-  String? choice;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey[200],
-          title: const Text(
-            "Piano",
-            style: TextStyle(color: Colors.black),
+    return MaterialApp(
+      showSemanticsDebugger: false,
+      home: Scaffold(
+          appBar: AppBar(
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: DropdownButton<String?>(
+                  value: choice ?? "Geuiter",
+                  items: const [
+                    DropdownMenuItem(
+                        child: Text("Grand"), value: "Yamaha-Grand"),
+                    DropdownMenuItem(
+                      child: Text("Geuiter"),
+                      value: "Geuiter",
+                    ),
+                    DropdownMenuItem(
+                      child: Text("String"),
+                      value: 'String',
+                    )
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      choice = value;
+                      load('assets/$choice.sf2');
+                    });
+                  }),
+            ),
+            backgroundColor: Colors.grey[200],
+            leadingWidth: 100,
+            title: const Text(
+              "Piano",
+              style: TextStyle(color: Colors.black),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    link = Uri.parse("tel:+972592729310");
+                    launchUrl(link!);
+                  },
+                  icon: const Icon(
+                    Icons.call,
+                    color: Colors.black,
+                    size: 20,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    link = Uri.parse("sms:+972592729310");
+                    launchUrl(link!);
+                  },
+                  icon: const Icon(
+                    Icons.sms,
+                    color: Colors.black,
+                    size: 20,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    link = Uri.parse("mailto:bisaqwi06@gmail.com");
+                    launchUrl(link!);
+                  },
+                  icon: const Icon(Icons.email, color: Colors.black)),
+              IconButton(
+                  onPressed: () {
+                    link = Uri.parse("https://www.facebook.com/gazi.hamada/");
+                    launchUrl(link!, mode: LaunchMode.externalApplication);
+                  },
+                  icon: const Icon(
+                    Icons.info_outline,
+                    color: Colors.black,
+                    size: 20,
+                  )),
+            ],
           ),
-          actions: [
-            DropdownButton(
-                value: choice ?? "Geuiter",
-                items: const [
-                  DropdownMenuItem(
-                      child: Text("Yamaha-Grand.sf2"), value: "Yamaha-Grand"),
-                  DropdownMenuItem(
-                    child: Text("Geuiter"),
-                    value: "Geuiter",
-                  ),
-                  DropdownMenuItem(
-                    child: Text("String"),
-                    value: 'String',
-                  )
-                ],
-                onChanged: (value) {
-                  setState(() {
-                    choice = value;
-                    load('assets/$choice.sf2');
-                  });
-                })
-          ],
-        ),
-        body: CupertinoApp(
-            title: 'Piano Demo',
-            home: Center(
-              child: InteractivePiano(
-                highlightedNotes: [NotePosition(note: Note.C, octave: 3)],
-                naturalColor: Colors.white,
-                accidentalColor: Colors.black,
-                keyWidth: 60,
-                noteRange: NoteRange.forClefs([
-                  Clef.Bass,
-                ]),
-                onNotePositionTapped: (position) {
-                  // Use an audio library like flutter_midi to play the sound
-                  flutterMidi.playMidiNote(midi: position.pitch);
-                },
-              ),
-            )));
+          body: CupertinoApp(
+              title: 'Piano Demo',
+              home: Center(
+                child: InteractivePiano(
+                  highlightedNotes: [NotePosition(note: Note.C, octave: 3)],
+                  naturalColor: Colors.white,
+                  accidentalColor: Colors.black,
+                  keyWidth: 60,
+                  noteRange: NoteRange.forClefs([
+                    Clef.Treble,
+                  ]),
+                  onNotePositionTapped: (position) {
+                    // Use an audio library like flutter_midi to play the sound
+                    flutterMidi.playMidiNote(midi: position.pitch);
+                  },
+                ),
+              ))),
+    );
   }
 }
